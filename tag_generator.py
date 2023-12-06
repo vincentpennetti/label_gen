4bbecae7d5605f6b12387f16b17a8d6b1bb73225
Vincent <95982347+vincentpennetti@users.noreply.github.com>
2023-12-06 15:25:16 -0500

    Update tag_generator.py
    Modified to properly scale titles on the label and correct their vertical spacing

--- tag_generator.py
+++ tag_generator.py
@@ -1,47 +1,54 @@
 from flask import Flask, request, send_file, Response
 from ppf.datamatrix import DataMatrix
 from PIL import Image, ImageDraw, ImageFont
-from io import BytesIO, StringIO
+from io import BytesIO
 import os
-from urllib.request import urlopen
-from werkzeug.wsgi import FileWrapper, wrap_file
+from werkzeug.wsgi import FileWrapper
 
+from typing import Tuple
 
+def calculate_font_size(draw, text: str, font_path: str, max_width: int, output_height: int, padding: int) -> Tuple[int, ImageFont.FreeTypeFont]:
+    font_size = int((output_height - 3 * padding) / 4)
 
-# http://127.0.0.1:5000/generate?p=Tddsasddd&s=xxx&t=nadfadf&q=mmM
+    while font_size > 0:
+        temp_font = ImageFont.truetype(font_path, layout_engine=ImageFont.LAYOUT_BASIC, size=font_size)
+
+        # Get the total width and height of the title
+        title_width, title_height = draw.textsize(text, font=temp_font)
+
+        if title_width < max_width:
+            return font_size, title_height, temp_font
+
+        font_size -= 1
+
+    # Return default values if none is found
+    return 12, 0, ImageFont.load_default()
 
 app = Flask(__name__)
-
-
-@app.route('/tmp')
-def hello():
-    return "Hello world!"
-
 
 @app.route('/')
 def generate():
     # Get parameters from the URL
+    dm = request.args.get('dm')
     primary_title = request.args.get('p')
     secondary_title = request.args.get('s')
     tertiary_title = request.args.get('t')
     quaternary_title = request.args.get('q')
+    quinary_title = request.args.get('a5')
     initials = request.args.get('i')
 
-    # could just \n each of the subtitles?
-    # tertiary_title = f"{tertiary_title} \n {initials}"
-
+    # Combine tertiary_title and initials
     tertiary_title = f"{tertiary_title} {initials}"
 
     # Generate the datamatrix code
-    myDataMatrix = DataMatrix(primary_title)
+    myDataMatrix = DataMatrix(dm)
 
     # Set the desired size of the output image
-    output_width = 1200
+    output_width = 2400
     output_height = output_width // 2
 
     datamatrix_width = output_width // 4
     datamatrix_height = datamatrix_width
-    title_x = output_width // 3
     padding = output_width // 48
 
     available_width = output_width - (datamatrix_width + (padding))
@@ -60,51 +67,68 @@ def generate():
 
     # Add titles to the right of the datamatrix
     draw = ImageDraw.Draw(canvas)
-    font_path = os.path.join(os.path.dirname(__file__), 'ArialBold.ttf')
-    print(font_path)
+    font_path_bold = os.path.join(os.path.dirname(__file__), 'ArialBold.ttf')
+    font_path = os.path.join(os.path.dirname(__file__), 'Arial.ttf')
 
-    # Determine font size for primary title based on available width
-    primary_title_size = int(((output_height - 3 * padding)) / 4)
-    while primary_title_size > 0:
-        temp_font = ImageFont.truetype(font_path, layout_engine=ImageFont.LAYOUT_BASIC, size=primary_title_size)
-        if draw.textbbox((0, 0), primary_title, font=temp_font)[2] < available_width:
-            primary_title_font = temp_font
-        break
-        primary_title_size -= 1
+    # Determine font sizes and heights for all titles
+    max_primary_title_width = available_width - (padding * 2)
+    primary_title_size, primary_title_height, primary_title_font = calculate_font_size(draw, primary_title, font_path_bold, max_primary_title_width, output_height, padding)
+    
+    max_secondary_title_width = available_width - (padding * 2)
+    secondary_title_size, secondary_title_height, secondary_title_font = calculate_font_size(draw, secondary_title, font_path, max_secondary_title_width, output_height - primary_title_size - padding, padding)
+    
+    max_tertiary_title_width = available_width - (padding * 2)
+    tertiary_title_size, tertiary_title_height, tertiary_title_font = calculate_font_size(draw, tertiary_title, font_path, max_tertiary_title_width, output_height - primary_title_size - padding, padding)
+    
+    max_quaternary_title_width = available_width - (padding * 2)
+    quaternary_title_size, quaternary_title_height, quaternary_title_font = calculate_font_size(draw, quaternary_title, font_path, max_quaternary_title_width, output_height - primary_title_size - padding, padding)
+    
+    max_quinary_title_width = available_width - (padding * 2)
+    quinary_title_size, quinary_title_height, quinary_title_font = calculate_font_size(draw, quinary_title, font_path, max_quinary_title_width, output_height - primary_title_size - padding, padding)
+
     # Set font styles
-    secondary_title_font = ImageFont.truetype(font_path, layout_engine=ImageFont.LAYOUT_BASIC, size=int(((output_height) - primary_title_size) / 4))
-    tertiary_title_font = ImageFont.truetype(font_path, layout_engine=ImageFont.LAYOUT_BASIC, size=int(((output_height) - primary_title_size) / 4))
-    quaternary_title_font = ImageFont.truetype(font_path, layout_engine=ImageFont.LAYOUT_BASIC, size=int(((output_height) - primary_title_size) / 4))
+    secondary_title_font = ImageFont.truetype(font_path, size=secondary_title_size)
+    tertiary_title_font = ImageFont.truetype(font_path, size=tertiary_title_size)
+    quaternary_title_font = ImageFont.truetype(font_path, size=quaternary_title_size)
+    quinary_title_font = ImageFont.truetype(font_path, size=quinary_title_size)
 
-    # Calculate y positions for secondary, tertiary, and quaternary titles
-    line_spacing = int(((output_height) - primary_title_size) / 4)
-    primary_title_y = 0  # - 19
-    secondary_title_y = primary_title_y + primary_title_size
-    tertiary_title_y = secondary_title_y + line_spacing
-    quaternary_title_y = tertiary_title_y + line_spacing
-
+    # Calculate total height occupied by titles
+    total_height = primary_title_height + secondary_title_height + tertiary_title_height + quaternary_title_height + quinary_title_height
+    
+    # Calculate line spacing for single spacing
+    line_spacing = int((output_height - total_height) / 5)
+    
+    # Calculate y positions for titles with single spacing
+    primary_title_y = 0
+    secondary_title_y = primary_title_y + primary_title_height + line_spacing
+    tertiary_title_y = secondary_title_y + secondary_title_height + line_spacing
+    quaternary_title_y = tertiary_title_y + tertiary_title_height + line_spacing
+    quinary_title_y = quaternary_title_y + quaternary_title_height + line_spacing
+    
     # Print titles
     draw.text((datamatrix_width + padding * 2, primary_title_y), primary_title, font=primary_title_font, fill=0)
     draw.text((datamatrix_width + padding * 2, secondary_title_y), secondary_title, font=secondary_title_font, fill=0)
     draw.text((datamatrix_width + padding * 2, tertiary_title_y), tertiary_title, font=tertiary_title_font, fill=0)
     draw.text((datamatrix_width + padding * 2, quaternary_title_y), quaternary_title, font=quaternary_title_font,
               fill=0)
+    draw.text((datamatrix_width + padding * 2, quinary_title_y), quinary_title, font=quinary_title_font,
+              fill=0)
 
     # Save the image to a buffer
     buffer = BytesIO()
     canvas.save(buffer, "PNG")
     buffer.seek(0)
+
+    # Set the filename for the downloaded image
+    filename = "label.png"
+
+    # Create a FileWrapper and return a Response with the appropriate headers
     w = FileWrapper(buffer)
-    return Response(w, mimetype="image/png", direct_passthrough=True)
+    response = Response(w, mimetype="image/png", direct_passthrough=True)
+    response.headers.set('Content-Disposition', 'attachment', filename=filename)
 
-    #canvas.save(buffer, "PNG")
-    #buffer.seek(0)
 
-    # Return the image to the user
-    #return send_file(buffer, mimetype="image/png")
-
+    return response
 
 if __name__ == "__main__":
     app.run()
-    
-    
